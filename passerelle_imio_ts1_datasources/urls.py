c23b80c6139616352f84b6d8f5b9bfeb3ea0554e
Christophe Boulanger <christophe.boulanger@imio.be>
2017-02-28 13:30:21 +0100

    Add motivationterme delete method (model + view)

--- passerelle_imio_ts1_datasources/urls.py
+++ passerelle_imio_ts1_datasources/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from .views import DatasourcesView, MotivationtermAddView
+from .views import DatasourcesView, MotivationtermAddView, MotivationtermDeleteView
 
 urlpatterns = patterns('',
     url(r'^(?P<slug>[\w,-]+)/data$', DatasourcesView.as_view(), name='DatasourcesView-data'),
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
 management_urlpatterns = patterns('',
     url(r'^(?P<connector_slug>[\w,-]+)/motivationterm/add/',
 	MotivationtermAddView.as_view(), name='motivationterm-add'),
+    url(r'^(?P<connector_slug>[\w,-]+)/motivationterm/(?P<pk>[\w,-]+)/delete/',
+	MotivationtermDeleteView.as_view(), name='motivationterm-delete'),
 )
--- passerelle_imio_ts1_datasources/views.py
+++ passerelle_imio_ts1_datasources/views.py
@@ -1,5 +1,4 @@
-# from django.http import HttpResponseRedirect
-from django.views.generic import View, CreateView
+from django.views.generic import View, CreateView, DeleteView
 from django.views.generic.detail import SingleObjectMixin
 from django.core.urlresolvers import reverse
 
@@ -8,15 +7,22 @@ from .models import ImioTs1Datasources, MotivationTerm
 
 class DatasourcesView(View, SingleObjectMixin):
     model = ImioTs1Datasources
-    # form_class = DatasourcesForm
 
 
-# DatasourcesAddView is missing a QuerySet. Define DatasourcesAddView.model,
-# DatasourcesAddView.queryset, or override DatasourcesAddView.get_queryset().
 class MotivationtermAddView(CreateView):
     model = MotivationTerm
     fields = '__all__'
     template_name = 'passerelle_imio_ts1_datasources/motivationterm_form.html'
+
+    def get_success_url(self):
+        connector = ImioTs1Datasources.objects.get(slug=self.kwargs['connector_slug'])
+        return reverse('view-connector',
+                kwargs={'connector': connector.get_connector_slug(),
+                        'slug': connector.slug})
+
+
+class MotivationtermDeleteView(DeleteView):
+    model = MotivationTerm
 
     def get_success_url(self):
         connector = ImioTs1Datasources.objects.get(slug=self.kwargs['connector_slug'])
